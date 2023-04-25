eddd020ce56fc24d77a97c0000c251debf13d76d
Hasin Mahmood <hasinmahmood01@gmail.com>
2023-04-25 23:21:11 +0100

    made end points for posting and getting tickets Only get tickets that are for sale

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const supabase = require("../database/connection");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'bye' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "bye" });
+});
+
+router.get("/tickets", async function (req, res, next) {
+  const { data, error } = await supabase
+    .from("ticketing")
+    .select()
+    .eq("for_sale", true);
+  if (error) {
+    res.send(error);
+  } else {
+    console.log("sent");
+    res.send(data);
+  }
+});
+
+router.post("/tickets", async function (req, res, next) {
+  const { data, error } = await supabase
+    .from("ticketing")
+    .insert(req.body)
+    .select();
+  console.log(req.body);
+  if (error) {
+    res.send(error);
+  } else {
+    console.log("sent");
+    res.send(data);
+  }
 });
 
 module.exports = router;
